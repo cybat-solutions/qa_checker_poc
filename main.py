01851bf2cb78738457540a62ae4b071f957ff785
Axel <simaxel@live.fr>
2022-10-27 16:41:06 +0200

    put env var in variable

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 
 df = pd.DataFrame(columns=["a", "b", "c"], data=[[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
+VALUE_TEST = os.environ["VALUE_TEST"]
 
 if __name__ == "__main__":
     print("Hello World!")
     print(df.shape)
     print(df.info())
     print(df)
-    print(os.environ.get("VALUE_TEST"))
+    print(VALUE_TEST)
