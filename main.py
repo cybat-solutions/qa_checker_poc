444a1c179cca89f47b4f63df3d76373c19db2c02
Axel <simaxel@live.fr>
2022-10-27 16:29:57 +0200

    add env variable

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import os
 
 df = pd.DataFrame(columns=["a", "b", "c"], data=[[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
 
 if __name__ == "__main__":
     print("Hello World!")
     print(df.shape)
     print(df.info())
     print(df)
+    print(os.environ.get("VALUE_TEST"))
