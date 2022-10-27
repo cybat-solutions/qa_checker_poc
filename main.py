9d335db3ab1af278f5085d993adb6a0fe5a33c0c
Axel <simaxel@live.fr>
2022-10-27 16:07:40 +0200

    add pandas use for requirements test

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import pandas as pd
+
+df = pd.DataFrame(columns=["a", "b", "c"], data=[[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
 if __name__ == "__main__":
     print("Hello World!")
+    print(df.shape)
+    print(df.info())
+    print(df)
