cfa07f1e1cece2d4598c612a4a5caba739cd0f75
Axel <simaxel@live.fr>
2022-10-27 17:25:12 +0200

    test env var handling

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ df = pd.DataFrame(columns=["a", "b", "c"], data=[[1, 2, 3], [4, 5, 6], [7, 8, 9]
 
 VALUE_TEST = os.environ["VALUE_TEST"]
 
+SHAREPOINT_PASSWORD = "ok boomer"
+
 if __name__ == "__main__":
     print("Hello World!")
     print(df.shape)
     print(df.info())
     print(df)
+    print(SHAREPOINT_PASSWORD)
     if VALUE_TEST == "test":
         print(VALUE_TEST, "is equal to test")
     else:
