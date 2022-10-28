f4026865c6108bd434603eb5010373d4ac1d2fe6
Axel <simaxel@live.fr>
2022-10-28 15:48:48 +0200

    test logger handling

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
 import pandas as pd
 import os
 
+from time import sleep
+
+import logging
+
 df = pd.DataFrame(columns=["a", "b", "c"], data=[[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-VALUE_TEST = os.environ["VALUE_TEST"]
+VALUE_TEST = os.environ.get("VALUE_TEST", "test")
 SHAREPOINT_PASSWORD = "ok boomer"
 
 counter = 0
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord):
+        head = getattr(record, "head", None)
+        body = getattr(record, "body", None)
+        if not head and not body:
+            return super().format(record)
+
+        outputtype = "notice"
+
+        if record.levelno == logging.DEBUG:
+            outputtype = "debug"
+
+        elif record.levelno == logging.WARNING:
+            outputtype = "warning"
+
+        elif record.levelno in (logging.ERROR, logging.CRITICAL):
+            outputtype = "error"
+
+            if record.exc_info:
+                # print the traceback and the exception name and details
+                logger.exception(record.msg)
+
+        return (
+                f"::{outputtype}" + ('' if head is None else f" title={head}")
+                + "::" + (body or record.msg)
+        )
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+handler.setFormatter(CustomFormatter())
+logger.addHandler(handler)
 
 if __name__ == "__main__":
     print("Hello World!")
@@ -17,4 +56,6 @@ if __name__ == "__main__":
 
     while counter != 5:
         print(counter)
+        logger.info(f"counter: {counter}")
+        sleep(1)
         counter += 1
